0020338854e757bb535fba2ff5b609c1b6740b33
minoaw <minoaw@gmail.com>
2019-04-09 00:09:04 +0000

    Modify spec pathes

--- wdio.conf.js
+++ wdio.conf.js
@@ -20,8 +20,7 @@ exports.config = {
     // directory is where your package.json resides, so `wdio` will be called from there.
     //
     specs: [
-        './test/specs/**/*.js',
-        './test/specs/**/*.ts'
+        './test/specs/**/*-test.ts'
     ],
     // Patterns to exclude.
     exclude: [
